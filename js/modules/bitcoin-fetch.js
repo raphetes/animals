150013ae4b9061f87fd24bb8522bf2cc7480f4f8
Raphael Melo <raatzraatz@gmail.com>
2025-06-25 14:00:07 -0300

    fetch-bitocin refatorado

--- js/modules/bitcoin-fetch.js
+++ js/modules/bitcoin-fetch.js
@@ -1,11 +1,8 @@
-export default function initBitcoinFetch() {
-  async function btcPrice(url) {
-    try {
-      const data = await fetch(url);
-      const btc = await data.json();
-      const btcSpan = document.querySelector(".btc-price");
-      btcSpan.innerText = (1000 / btc.BRL.sell).toFixed(4);
-    } catch (erro) {}
-  }
-  btcPrice("https://blockchain.info/ticker");
+export default async function fetchBitcoin(url, target) {
+  try {
+    const data = await fetch(url);
+    const btc = await data.json();
+    const btcSpan = document.querySelector(target);
+    btcSpan.innerText = (1000 / btc.BRL.sell).toFixed(4);
+  } catch (erro) {}
 }
