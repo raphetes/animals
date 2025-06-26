f126511a46f3a08dcb08d168360d9b7fe085c9e6
Raphael Melo <raatzraatz@gmail.com>
2025-06-25 22:16:35 -0300

    anim-scroll refatorado

--- js/modules/scrollAnimation.js
+++ js/modules/scrollAnimation.js
@@ -2,16 +2,30 @@ export default class ScrollAnimation {
   constructor(section) {
     this.sections = [...document.querySelectorAll(section)];
     this.windowHeight = window.innerHeight * 0.7;
-    this.scrollAnimation = this.scrollAnimation.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
-  scrollAnimation() {
-    this.sections.forEach((section) => {
-      const sectionTopDistance = section.getBoundingClientRect().top;
-      const verify = sectionTopDistance - this.windowHeight < 0;
-      if (verify) section.classList.add("ativo");
+  getDistance() {
+    this.distances = this.sections.map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.windowHeight,
+      };
+    });
+  }
+  checkDistance() {
+    this.distances.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      }
     });
   }
   init() {
-    window.addEventListener("scroll", this.scrollAnimation);
+    if (this.sections.length) {
+      window.addEventListener("scroll", this.checkDistance);
+      this.getDistance();
+      this.checkDistance();
+    }
+    return this;
   }
 }
